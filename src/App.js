900ff8074aa7156ccabda3fbc323dddade4b19b7
Saksham Puri <sakshampuri1999@gmail.com>
2020-05-30 22:43:47 +0530

    components excercise 2 supplying state to child component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import Menu from './components/MenuComponent';
 
-function App() {
-  return (
-    <div>
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Restorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu />
-    </div>
-  );
+import {DISHES} from './shared/dishes'
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes : DISHES
+    };
+  }
+
+  render() {
+    return(
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Restorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes = {this.state.dishes}/>
+      </div>
+    );
+  }
 }
 
 export default App;
